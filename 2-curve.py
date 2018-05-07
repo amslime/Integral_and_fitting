28c38640891fa744a96c8d9b6350e5c9076817fb
amslime <756433605@qq.com>
2018-05-07 11:20:27 +0800

    fix cases

--- 2-curve.py
+++ 2-curve.py
@@ -13,8 +13,9 @@ def mean(tuples, xt, yt, zt):
     n = len(tuples)
     sum = 0;
     for i in range(0, n):
-        sum += tuples[i][0]**xt + tuples[i][1]**yt + tuples[i][2]**zt
+        sum += tuples[i][0]**xt * tuples[i][1]**yt  * tuples[i][2]**zt
     sum = sum * 1.0 / n
+    print sum
     return sum
 
 
@@ -25,10 +26,11 @@ def createMatrix(tuples):
     m = []
     for i in range(0, 6):
         m_s1 = m1 + m1[i]
+        print m_s1
         for j in range(0, 6):
             m.append(mean(tuples, m_s1[j][0],m_s1[j][1], m_s1[j][2]))
     m_n = np.asmatrix(np.array(m).reshape(6,6))
-    #print m_n
+    print m_n
     return m_n
 
 def createArray(tuples):
@@ -42,6 +44,7 @@ def createArray(tuples):
 def solve(tuples):
     mat1 = createMatrix(tuples)
     arr1 = createArray(tuples)
+    print mat1, arr1
     return np.linalg.solve(mat1, arr1)
 
 def check(x, y, z, ans):
@@ -51,15 +54,13 @@ def check(x, y, z, ans):
     
 
 
-tuples = [(6, 5, 8), (4, 1, 7), (3,9,2)]
 
-print mean(tuples, 2, 0, 1)
+def testEqu(x, y):
+    return (x, y, x * x + 2 * y * y - 3 * x * y + 4)
 
-a = np.array([1,2,3])
-b = np.array([4,5,6])
-#print np.append(a,b).reshape(3,2)
-
-m1 = np.array([[2,1,1],[2,0,1]])
-#print m1 + np.array([1,1,1])
-
+# z = x^2 + 2y^2 - 3xy + x + 1
+tuples = [testEqu(1,2), testEqu(3,5), testEqu(9,3), testEqu(14,5), testEqu(27,6), testEqu(68,13), testEqu(13,21), testEqu(66, 63), testEqu(32,51)]
 ans = solve(tuples)
+print ans
+t1 = testEqu(54,43)
+check(t1[0], t1[1], t1[2],  ans)
